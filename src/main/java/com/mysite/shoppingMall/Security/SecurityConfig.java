a7202a1e0e50fd7c4779872d0e55e18a807c1f21
denist2322 <parksomang125@gmail.com>
2022-08-17 10:44:43 +0900

    검색 키워드 막음

--- src/main/java/com/mysite/shoppingMall/Security/SecurityConfig.java
+++ src/main/java/com/mysite/shoppingMall/Security/SecurityConfig.java
@@ -16,8 +16,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll()
-        ;
+        http.authorizeRequests().antMatchers("/**").permitAll();        ;
         return http.build();
     }
 
