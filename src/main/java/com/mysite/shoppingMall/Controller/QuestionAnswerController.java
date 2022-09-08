bbf2a9e2e1a288e970b52220492197bf368a5ef5
leehaneul623 <dyj03216@naver.com>
2022-09-08 13:06:23 +0900

    답변등록 주석

--- src/main/java/com/mysite/shoppingMall/Controller/QuestionAnswerController.java
+++ src/main/java/com/mysite/shoppingMall/Controller/QuestionAnswerController.java
@@ -23,6 +23,7 @@ public class QuestionAnswerController {
         Question question = this.questionService.getQuestion(id); // 아이디를 찾아와서
         this.questionAnswerService.create(question, body); // 서비스에 일을시켜 답변을 등록한다.
 
+        
         return String.format("redirect:/question/detail/%s", id);
     }
 
