846a8aef130e4d6aa7944fde5464320c5a04763d
ohyoungkyu <come8908@gmail.com>
2022-09-16 10:52:09 +0900

    js 생성 및 js랑 html 연결

---
+++ src/main/resources/static/common/Swiper.js
@@ -0,0 +1,19 @@
+
+  var swiper = new Swiper(".mySwiper", {
+  loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
